19eb13cbc7ff5f05b06d250b70785f5b7d9dc267
baileykc <baileykc@rose-hulman.edu>
2019-04-01 19:03:34 -0400

    m4 done

--- src/m4_sequences.py
+++ src/m4_sequences.py
@@ -304,7 +304,7 @@ def practice_problem4c(points):
       :rtype: rg.Point | string
     """
     ###########################################################################
-    # TODO: 4. Implement and test this function.
+    # Done: 4. Implement and test this function.
     #     The testing code is already written for you (above).
     #
     # IMPORTANT: This problem is your LOWEST PRIORITY for preparing
@@ -315,8 +315,16 @@ def practice_problem4c(points):
     #    DIFFICULTY:      9
     #    TIME ESTIMATE:   15 minutes.
     ###########################################################################
+    for k in range(len(points)):
+        if is_prime(points[k].x) == True:
+            if is_prime(points[k].y) == True:
+                x = points[k].x
+                y = points[k].y
+                points[k].x = y
+                points[k].y = x
+                return rg.Point(y, x)
 
-
+    return "Not found"
 def run_test_practice_problem4d():
     """ Tests the    practice_problem4d    function. """
     # -------------------------------------------------------------------------
@@ -400,14 +408,21 @@ def practice_problem4d(sequence):
       :rtype: int
     """
     ###########################################################################
-    # TODO: 5. Implement and test this function.
+    # Done: 5. Implement and test this function.
     #     The testing code is already written for you (above).
     ###########################################################################
     # DIFFICULTY AND TIME RATINGS (see top of this file for explanation)
     #    DIFFICULTY:      7
     #    TIME ESTIMATE:   15 minutes.
     ###########################################################################
-
+    total = 0
+    for k in range(len(sequence) - 1):
+        if is_prime(sequence[k]) == True:
+            if is_prime(sequence[k + 1]) == True:
+                if is_prime(sequence[k + 1]) == True:
+                    if sequence[k] != sequence[k + 1]:
+                        total = total + sequence[k]
+    return total
 
 # -----------------------------------------------------------------------------
 # Calls  main  to start the ball rolling.
